623c7f117acfb66f76188e42dc2c81b9e97f3559
Fritz <furitsucode@gmail.com>
2022-12-01 06:44:09 +0700

    site

--- src/Solutions/product-preview-card-component/main.tsx
+++
@@ -1,9 +0,0 @@
-function ProductPreviewCardComponent() {
-  return (
-    <div className="ProductPreviewCardComponent">
-      <h3>PRODUCT PREVIEW CARD COMPONENT!</h3>
-    </div>
-  )
-}
-
-export default ProductPreviewCardComponent
